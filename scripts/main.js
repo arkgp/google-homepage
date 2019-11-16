1b88cff3079fd4b1a28d9840bddd1bc9540b2788
Gabriel Panetta <ark.gp20@gmail.com>
2019-11-16 15:19:21 -0500

    Improve readability

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,19 @@
 'use strict';
 
-document.getElementById("search-bar").focus()
+let searchDiv = document.getElementById("search-div"),
+    searchBar = document.getElementById("search-bar");
 
-function searchBarOnFocus() {
-  let searchDiv = document.getElementById("search-div");
-  searchDiv.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)";
-  searchDiv.style.borderColor = "rgba(223,225,229,0)";
-}
-
+searchBar.focus()
 searchBarOnFocus();
-
-let searchBar = document.getElementById("search-bar");
 
 searchBar.addEventListener('focus', searchBarOnFocus);
 
 searchBar.addEventListener('focusout', function() {
-    let searchDiv = document.getElementById("search-div");
     searchDiv.style.boxShadow = "none";
     searchDiv.style.borderColor = "rgb(223, 225, 229)";
 });
+
+function searchBarOnFocus() {
+  searchDiv.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)";
+  searchDiv.style.borderColor = "rgba(223,225,229,0)";
+}
